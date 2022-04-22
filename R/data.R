41e3e9e5b3531b5439114edea2f9aebb8e11ff94
W. Christohper Carleton <ccarleton@protonmail.com>
2022-04-22 14:44:50 +0200

    readme tinkering

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Palaeotemperature reconstruction time-series
+#'
+#' A time-series of pollen-based palaeotemperature estimates for North America
+#' spanning 6900--100 BP. The original series is available online at the NOAA
+#' data warehouse and was published in Viau et al. 2006 (doi:10.1029/
+#' 2005JD006031). These data are from the first table in the published tab-
+#' delimited data file and have been extracted for demonstration purposes only.
+#' Anyone interested in the full dataset for scientific reasons should see the
+#' original source.
+#'
+#' @format A data frame with 69 rows and 2 variables:
+#' \describe{
+#'   \item{Age BP}{Age, in years BP}
+#'   \item{Temperature}{Temperature estimate}
+#'   ...
+#' }
+#' @source \url{https://www.ncei.noaa.gov/pub/data/paleo/pollen/recons/northamerica/viau2006namerica-temp.txt}
